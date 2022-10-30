b69f6dedd0214adadae4d3203d9c524127129ba5
Federico Soler <solerfederico96@gmail.com>
2022-10-30 19:32:41 -0300

    nuevas clases

---
+++ SRC/Proyecto/El_Sabroso_App/CapaPresentacion/FrmVentas.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace El_Sabroso_App.CapaPresentacion
+{
+    partial class FrmVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboProducto = new System.Windows.Forms.ComboBox();
+            this.Producto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboCategoria = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CantProd = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MontoTotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboProveedor = new System.Windows.Forms.ComboBox();
+            this.nombreProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreProveedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ApellidoCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelefonoCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.CantProd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboProducto
+            // 
+            this.comboProducto.FormattingEnabled = true;
+            this.comboProducto.Location = new System.Drawing.Point(75, 66);
+            this.comboProducto.Name = "comboProducto";
+            this.comboProducto.Size = new System.Drawing.Size(121, 23);
+            this.comboProducto.TabIndex = 1;
+            // 
+            // Producto
+            // 
+            this.Producto.AutoSize = true;
+            this.Producto.Location = new System.Drawing.Point(8, 66);
+            this.Producto.Name = "Producto";
+            this.Producto.Size = new System.Drawing.Size(59, 15);
+            this.Producto.TabIndex = 2;
+            this.Producto.Text = "Producto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Categoria:";
+            // 
+            // comboCategoria
+            // 
+            this.comboCategoria.FormattingEnabled = true;
+            this.comboCategoria.Location = new System.Drawing.Point(76, 37);
+            this.comboCategoria.Name = "comboCategoria";
+            this.comboCategoria.Size = new System.Drawing.Size(121, 23);
+            this.comboCategoria.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(691, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(97, 23);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fecha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cantidad:";
+            // 
+            // CantProd
+            // 
+            this.CantProd.Location = new System.Drawing.Point(76, 7);
+            this.CantProd.Name = "CantProd";
+            this.CantProd.Size = new System.Drawing.Size(120, 23);
+            this.CantProd.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(708, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MontoTotal
+            // 
+            this.MontoTotal.Location = new System.Drawing.Point(602, 95);
+            this.MontoTotal.Name = "MontoTotal";
+            this.MontoTotal.Size = new System.Drawing.Size(100, 23);
+            this.MontoTotal.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(550, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Monto:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombreProducto,
+            this.nombreProveedor,
+            this.nombreCategoria,
+            this.Cantidad,
+            this.Cliente,
+            this.ApellidoCliente,
+            this.TelefonoCliente,
+            this.Mail});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(779, 272);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Nombre Cliente:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(322, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 23);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 23);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(223, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Apellido";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(322, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(159, 23);
+            this.textBox3.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(223, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Mail";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(322, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(159, 23);
+            this.textBox4.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(223, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Telefono";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Aceptar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 15);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Proveedor:";
+            // 
+            // comboProveedor
+            // 
+            this.comboProveedor.FormattingEnabled = true;
+            this.comboProveedor.Location = new System.Drawing.Point(74, 95);
+            this.comboProveedor.Name = "comboProveedor";
+            this.comboProveedor.Size = new System.Drawing.Size(121, 23);
+            this.comboProveedor.TabIndex = 23;
+            // 
+            // nombreProducto
+            // 
+            this.nombreProducto.HeaderText = "Producto";
+            this.nombreProducto.Name = "nombreProducto";
+            // 
+            // nombreProveedor
+            // 
+            this.nombreProveedor.HeaderText = "Proveedor";
+            this.nombreProveedor.Name = "nombreProveedor";
+            // 
+            // nombreCategoria
+            // 
+            this.nombreCategoria.HeaderText = "Categoria";
+            this.nombreCategoria.Name = "nombreCategoria";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Cliente
+            // 
+            this.Cliente.HeaderText = "Cliente";
+            this.Cliente.Name = "Cliente";
+            // 
+            // ApellidoCliente
+            // 
+            this.ApellidoCliente.HeaderText = "ApellidoCliente";
+            this.ApellidoCliente.Name = "ApellidoCliente";
+            // 
+            // TelefonoCliente
+            // 
+            this.TelefonoCliente.HeaderText = "Telefono";
+            this.TelefonoCliente.Name = "TelefonoCliente";
+            // 
+            // Mail
+            // 
+            this.Mail.HeaderText = "Mail";
+            this.Mail.Name = "Mail";
+            // 
+            // FrmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboProveedor);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MontoTotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CantProd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboCategoria);
+            this.Controls.Add(this.Producto);
+            this.Controls.Add(this.comboProducto);
+            this.Name = "FrmVentas";
+            this.Text = "Ventas";
+            this.Load += new System.EventHandler(this.FrmVentas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CantProd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ComboBox comboProducto;
+        private Label Producto;
+        private Label label1;
+        private ComboBox comboCategoria;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown CantProd;
+        private Button button1;
+        private TextBox MontoTotal;
+        private Label label4;
+        private DataGridView dataGridView1;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private Button button2;
+        private Button button3;
+        private Label label9;
+        private ComboBox comboProveedor;
+        private DataGridViewTextBoxColumn nombreProducto;
+        private DataGridViewTextBoxColumn nombreProveedor;
+        private DataGridViewTextBoxColumn nombreCategoria;
+        private DataGridViewTextBoxColumn Cantidad;
+        private DataGridViewTextBoxColumn Cliente;
+        private DataGridViewTextBoxColumn ApellidoCliente;
+        private DataGridViewTextBoxColumn TelefonoCliente;
+        private DataGridViewTextBoxColumn Mail;
+    }
+}
