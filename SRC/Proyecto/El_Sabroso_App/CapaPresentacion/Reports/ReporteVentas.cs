f312707f634b0a33111a498184dbf725537a8fca
Facundo Montenegro <facundo.montenegro7@hotmail.com>
2022-11-13 20:42:35 -0300

    Se modifica pantalla y se descarga extension para reporte

---
+++ SRC/Proyecto/El_Sabroso_App/CapaPresentacion/Reports/ReporteVentas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace El_Sabroso_App.CapaPresentacion.Reports
+{
+    public partial class ReporteVentas : Form
+    {
+        public ReporteVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteVentas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
